91bac88137ab4cd89f05bb3f889e7a0f9c774c87
FateofOblivion <steffonjones13@gmail.com>
2025-06-13 18:43:57 -0400

    have the base for get_letters num still need to  fix some errors

--- stats.py
+++ stats.py
@@ -8,5 +8,18 @@ def get_num_words():
     word_list = (get_book_text("/home/fateofoblivion/bookbot/books/frankenstein.txt")).split()
     num_words = len(word_list)
     print(f"{num_words} words found in the document")
+    
+
+    
+
+def num_of_letters(full_text):
+    letter_list = {}
+    for letters in full_text:
+        if letter_list[letters.lower()] == True and letter_list[letters.lower()] >= 1:
+            letter_list[letters.lower()] += 1
+        else:
+            letter_list[letters.lower()] = 1
+    return letter_list
 
 get_num_words()
+num_of_letters(get_book_text("/home/fateofoblivion/bookbot/books/frankenstein.txt"))
